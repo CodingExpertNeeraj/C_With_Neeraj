8c48a8f87c09a096d298042db6f0c37adef95ae7
Neeraj tiwari <nt98924@gmail.com>
2024-02-24 21:29:04 +0530

    first prgram in c language

--- first.c
+++ first.c
@@ -2,6 +2,6 @@
 int main()
 {
     /* code */
-    printf("Hello world This is Neeraj Tiwarioutoo ");
+    printf("Hello world This is Neeraj Tiwari.  This i ws doing practice in C# language.  this is my first programs ");
     return 0;
 }
